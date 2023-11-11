05de0868236afb74b1e68125458da31a9855ec32
ChaeyunK <iehate52@gmail.com>
2023-11-12 01:36:43 +0900

    ex04

--- main.c
+++ main.c
@@ -4,12 +4,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-char src[]="The worst things to eat before you sleep";
-char dst[100];
-
-strcpy(dst, src);
-
-printf("copied string : %s", dst);
-
-return 0;
+    char str[30] = "happy C programming";
+    printf("The length of \"%s\" : %i", str, strlen(str));
+    //실제로는 20개(널문자포함), but strlen은 우리 눈에 보이는 개수만 출력해줌.
+    return 0;
 }
